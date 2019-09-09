1c7ba16bc6576df29a35b59b33531380acdd424c
Tercio Jose <terciob19@hotmail.com>
2019-09-09 16:10:44 -0300

    .gitignore

--- locales/koKR.lua
+++ locales/koKR.lua
@@ -1,4 +1,87 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FlashTaskbarLocales", "koKR") 
 if not L then return end 
 
-@localization(locale="koKR", format="lua_additive_table")@
+L["STRING_ADD"] = "추가"
+L["STRING_ADDKEYWORD"] = "단어 추가"
+--[[Translation missing --]]
+--[[ L["STRING_BACKGROUND_SOUND"] = ""--]] 
+L["STRING_BAGSFULL"] = "가득 찬 가방"
+L["STRING_BAGSFULL_DESC"] = "전문 기술 제작 중 소지품 가방이 가득 차면 알립니다."
+L["STRING_BATTLEGROUND"] = "전장 종료"
+L["STRING_BATTLEGROUND_DESC"] = "전장이 종료되면 반짝입니다."
+L["STRING_BRAWLERS"] = "싸움꾼 조합 대기열"
+L["STRING_BRAWLERS_DESC"] = "싸움꾼 투기장에서 싸울 차례가 되면 알립니다."
+L["STRING_CAMPINGSETTINGS"] = "캠핑 설정"
+L["STRING_CHAT_FLYPOINTENDED"] = "비행 지점에 도착했습니다."
+L["STRING_CHATSCAN"] = "대화 탐색"
+L["STRING_CHATSCAN_DESC"] = "대화 이벤트에 대한 경보입니다."
+L["STRING_CHATSCAN_ENABLED"] = "대화 탐색 사용"
+L["STRING_CHATSCAN_ENABLED_DESC"] = [=[대화에서 특정 단어를 검색합니다.
+
+예를 들어 특정 npc의 외침에 대한 경보를 원할 때 사용합니다.]=]
+L["STRING_CLOSESOUNDPANEL"] = "닫기"
+L["STRING_COMBATLOGSCAN"] = "전투기록 탐색"
+L["STRING_COMBATLOGSCAN_DESC"] = "전투 기록 이벤트에 대한 경보입니다."
+L["STRING_COMBATLOGSCAN_ENABLED"] = "전투기록 탐색 사용"
+L["STRING_COMBATLOGSCAN_ENABLED_DESC"] = [=[전투 기록에서 대상의 이름을 확인합니다.
+
+예를 들어 특정 npc가 피해를 받으면 경보로 알립니다.]=]
+L["STRING_DISCONNECT"] = "접속 종료"
+L["STRING_DISCONNECT_DESC"] = [=[당신의 캐릭터가 게임에서 접속이 종료되면 알립니다.
+
+자리 비움으로 인해 긴 대기열을 놓치지 않기 위해 중요합니다.]=]
+L["STRING_DUELREQUEST"] = "결투 요청"
+L["STRING_DUELREQUEST_DESC"] = "누군가 당신에게 애완동물 대전이나 결투 신청을 하면 알립니다."
+L["STRING_ENTERCOMBAT"] = "전투 시작"
+L["STRING_ENTERCOMBAT_DESC"] = "캐릭터가 전투 상태가 되면 알립니다."
+L["STRING_ERASEKEYWORD"] = "단어 삭제"
+L["STRING_FATIGUE"] = "피로"
+L["STRING_FATIGUE_DESC"] = "캐릭터가 지도를 벗어나 1분 피로도 초읽기가 시작되면 알립니다."
+L["STRING_FINDERQUEUES"] = "찾기 대기열"
+L["STRING_FINDERQUEUES_DESC"] = "공격대 찾기, 던전 찾기 구성 완료 시 작업 표시줄을 반짝입니다."
+L["STRING_FLYPOINT"] = "비행 지점 도착"
+L["STRING_FLYPOINT_DESC"] = "비행 조련사를 통한 비행이 끝나면 알립니다."
+L["STRING_GENERALSETTINGS"] = "일반 설정"
+L["STRING_INVITEIGNORE"] = "자동 수락 시 무시"
+L["STRING_INVITEIGNORE_DESC"] = "다른 애드온이 초대 요청에 자동으로 수락하면 반짝이지 않게 합니다."
+L["STRING_INVITES"] = "초대"
+L["STRING_INVITES_DESC"] = "누군가 당신을 그룹에 초대하면 알립니다."
+L["STRING_ONCOUNTDOWN"] = "타이머 시작"
+L["STRING_ONCOUNTDOWN_DESC"] = "화면에 초읽기가 표시되면 알립니다."
+--[[Translation missing --]]
+--[[ L["STRING_ONPLAYERDEATH"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["STRING_ONPLAYERDEATH_DESC"] = ""--]] 
+L["STRING_ONWHISPER"] = "귓속말 수신"
+L["STRING_ONWHISPER_DESC"] = "귓속말을 받으면 반짝입니다"
+L["STRING_OPENSOUNDPANEL"] = "소리 설정 열기"
+L["STRING_PETBATTLES"] = "애완동물 대전 대기열"
+L["STRING_PETBATTLES_DESC"] = "애완동물 대전 참여 대기 시 알립니다."
+L["STRING_PLAYERNAME"] = "대화에 이름"
+L["STRING_PLAYERNAME_DESC"] = "누군가 대화로 당신의 캐릭터 이름을 말하면 알립니다."
+L["STRING_PULL"] = "전투 예정 타이머"
+L["STRING_PULL_DESC"] = "BigWigs나 DBM으로부터 전투 예정 타이머가 시작되면 알립니다."
+L["STRING_PVPQUEUES"] = "PvP 대기열"
+L["STRING_PVPQUEUES_DESC"] = "전장과 투기장 참여 대기 시 작업 표시줄을 반짝입니다."
+L["STRING_RARENPCSCAN"] = "희귀 Npc 탐색"
+L["STRING_RARENPCSCAN_ANYNPC"] = "모든 희귀 Npc"
+L["STRING_RARENPCSCAN_DESC"] = "희귀 npc가 발견되면 경보로 알립니다."
+L["STRING_RARENPCSCAN_ENABLED"] = "희귀 탐색 사용"
+L["STRING_RARENPCSCAN_NPCNAME"] = "Npc 이름"
+L["STRING_READYCHECK"] = "전투 준비"
+L["STRING_READYCHECK_DESC"] = "전투 준비 시 작업 표시줄을 반짝입니다."
+L["STRING_REMOVE"] = "제거"
+L["STRING_REMOVE_TITLE"] = "제거"
+L["STRING_SOUNDSETTINGS"] = "소리 설정"
+L["STRING_SOUNDSETTINGS_DESC"] = "다음 상황에서 반짝임과 함께 소리 효과를 재생합니다"
+L["STRING_SUMMON"] = "소환"
+L["STRING_SUMMON_DESC"] = "흑마법사가 당신을 소환하면 알립니다."
+L["STRING_TARGETLOSTHEALTH"] = "대상 생명력 손실"
+L["STRING_TARGETLOSTHEALTH_DESC"] = "대상이 생명력을 잃으면 반짝입니다."
+L["STRING_TARGETLOWHEALTH"] = "대상 생명력 낮음"
+L["STRING_TARGETLOWHEALTH_DESC"] = "대상의 생명력이 18% 이하가 되면 반짝입니다."
+L["STRING_TRADE"] = "거래"
+L["STRING_TRADE_DESC"] = "누군가 당신에게 거래를 요청하면 알립니다."
+L["STRING_WORLDPVP"] = "야외 PvP"
+L["STRING_WORLDPVP_DESC"] = "아쉬란같은 야외 PvP 참여 대기 시 알립니다."
+
